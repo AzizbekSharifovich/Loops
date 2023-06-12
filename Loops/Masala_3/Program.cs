1bccde0f93ec1c3dfeb9013a7d8c60f96a74ac57
Azizbek <azizbek0698@gmail.com>
2023-06-12 06:23:18 +0500

    3-masala ishlandi

---
+++ Loops/Masala_3/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using System.Diagnostics.Metrics;
+int sum = 0, n;
+
+Console.WriteLine("Enter the value of n:");
+int n = Convert.Toint32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    sum += i;
+
+Console.WriteLine(sum);
---
+++ Loops/masala-3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter the value of n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    sum += i;
+}
+Console.WriteLine(sum);
